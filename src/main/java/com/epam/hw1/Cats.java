90c1ce922365c35dfbc044a9880defde306b4a5f
Margot <southinferno@gmail.com>
2017-10-09 21:44:19 +0300

    add project for doing a homework

---
+++ src/main/java/com/epam/hw1/Cats.java
@@ -0,0 +1,7 @@
+package com.epam.hw1;
+
+/**
+ * Created by Admin on 009 09.10.17.
+ */
+public class Cats {
+}
---
+++ src/test/java/com/epam/hw1/CatsTests.java
@@ -0,0 +1,10 @@
+package com.epam.hw1;
+import org.junit.Test;
+
+/**
+ * Created by Admin on 009 09.10.17.
+ */
+public class CatsTests {
+
+
+}
