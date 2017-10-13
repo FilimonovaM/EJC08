f1549d2545ad2057de9e3ccfe306619ac3d3fcee
Margot <southinferno@gmail.com>
2017-10-13 18:41:28 +0300

    little formal editing of array

--- src/test/java/com/epam/hw2/TestOperationsWithEnteredSymbols.java
+++ src/test/java/com/epam/hw2/TestOperationsWithEnteredSymbols.java
@@ -1,13 +1,12 @@
 package com.epam.hw2;
 
-
 import org.junit.Assert;
 import org.junit.Test;
 
 public class TestOperationsWithEnteredSymbols {
 
     @Test
-    public void testChecker(){
+    public void testCheck() {
         OperationsWithEnteredSymbols operationsWithEnteredSymbols = new OperationsWithEnteredSymbols();
         Assert.assertEquals(0, operationsWithEnteredSymbols.check("0"));
         Assert.assertEquals(100, operationsWithEnteredSymbols.check("100"));
--- src/test/java/com/epam/hw2/TestTrafficLightsCalculator.java
+++ src/test/java/com/epam/hw2/TestTrafficLightsCalculator.java
@@ -4,7 +4,7 @@ package com.epam.hw2;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class TestTrafficLightsCalculators {
+public class TestTrafficLightsCalculator {
     TrafficLightsCalculator trafficLightsCalculator = new TrafficLightsCalculator();
 
     @Test
