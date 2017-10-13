b50354786b4d355973960427996c9906c1aca67f
Margot <southinferno@gmail.com>
2017-10-13 17:08:10 +0300

    class work => added test for method answer() of TrafficLightsCalculator

--- src/test/java/com/epam/hw2/TestTrafficLightsCalculators.java
+++ src/test/java/com/epam/hw2/TestTrafficLightsCalculators.java
@@ -1,17 +1,18 @@
 package com.epam.hw2;
 
-import junit.framework.AssertionFailedError;
+
 import org.junit.Assert;
 import org.junit.Test;
 
-/**
- * Created by Admin on 013 13.10.17.
- */
 public class TestTrafficLightsCalculators {
-    TrafficLights trafficLights = new TrafficLights();
+    TrafficLightsCalculator trafficLightsCalculator = new TrafficLightsCalculator();
 
     @Test
-    public void testAnswer(){
-
+     public void testAnswer(){
+        Assert.assertEquals("RED", trafficLightsCalculator.answer(0));
+        Assert.assertEquals("YELLOW", trafficLightsCalculator.answer(2));
+        Assert.assertEquals("YELLOW", trafficLightsCalculator.answer(13));
+        Assert.assertEquals("RED", trafficLightsCalculator.answer(60));
+        Assert.assertEquals("GREEN", trafficLightsCalculator.answer(66));
     }
 }
