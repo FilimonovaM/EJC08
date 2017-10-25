435b11699fc50d86f9634239d0ddc5e263d7382f
FilimonovaM <FilimonovaMargarita@mail.ru>
2017-10-25 20:21:57 +0300

    hw3 ===> added new packages

---
+++ src/main/java/com/epam/hw3/typesOfFlight/FlyByStrangeFantasies.java
@@ -0,0 +1,12 @@
+package com.epam.hw3.typesOfFlight;
+
+public class FlyByStrangeFantasies implements FlyBehavior {
+
+    /**
+     * uses for demonstration one of the types of fly behavior.
+     */
+    @Override
+    public void fly() {
+        System.out.println("\t\"And yet the REAL man must be able to fly!\"");
+    }
+}
