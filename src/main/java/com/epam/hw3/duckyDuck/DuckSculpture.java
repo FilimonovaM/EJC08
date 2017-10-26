39902e3e2d57407c2c6e3fe0188eef250fb34c2f
FilimonovaM <FilimonovaMargarita@mail.ru>
2017-10-26 18:15:25 +0300

    hw3 ===> added new packages

--- src/main/java/com/epam/hw3/duckyDuck/DuckSculpture.java
+++ src/main/java/com/epam/hw3/duckyDuck/DuckSculpture.java
@@ -1,6 +1,15 @@
 package com.epam.hw3.duckyDuck;
 
+import com.epam.hw3.typesOfFlight.FlyWithWings;
+import com.epam.hw3.typesOfNoise.Quak;
+import com.epam.hw3.typesOfSwimming.SwimLikeAChampion;
+
 public class DuckSculpture extends Duck {
+    public DuckSculpture() {
+        makeASoundBehavior = new Quak();
+        flyBehavior = new FlyWithWings();
+        swimBehavior = new SwimLikeAChampion();
+    }
 
     /**
      * uses for personal presentation of the classes which extends from the superclass HomoSapience.
