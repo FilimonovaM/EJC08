066769c2be6aff4635eeb45a5db3e5d446be990d
FilimonovaM <FilimonovaMargarita@mail.ru>
2017-12-16 21:01:43 +0300

    JDBC => ex2 almost done

--- src/main/java/com/epam/vol2/jdbc/ex2/Behavior.java
+++
@@ -1,9 +0,0 @@
-package com.epam.vol2.jdbc.ex2;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-public interface Behavior {
-    PreparedStatement doIt(Connection connection) throws SQLException;
-}
--- src/main/java/com/epam/vol2/jdbc/ex2/JDBCStarter.java
+++
@@ -1,7 +0,0 @@
-package com.epam.vol2.jdbc.ex2;
-
-public class JDBCStarter {
-    public static void main(String[] args) {
-
-    }
-}
